a2967ef6eeba90c1d5815cf34ee99dc2ae034d7b
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-08-29 11:20:27 +0300

    define user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(
+        max_length=20,
+        validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$')],
+        blank=True
+    )
+    date_of_birth = models.DateField(null=True,blank=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username','first_name','last_name']
+
+    class Meta:
+        db_table = 'users',
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}".strip()
