901536902185afdebe73b6619e04c63783724469
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-08-29 13:38:22 +0300

    Address Detail view

--- users/views.py
+++ users/views.py
@@ -26,3 +26,11 @@ class AddressListCreateView(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
+class AddressDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Address.objects.filter(user=self.request.user)
+
+
