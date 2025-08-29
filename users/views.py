0256452fde9bc6cc5feb0f5ce0755de451d3535e
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-08-29 13:36:54 +0300

    Address List and Create

--- users/views.py
+++ users/views.py
@@ -15,3 +15,14 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
+class AddressListCreateView(generics.ListCreateAPIView):
+    serializer_class = AddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Address.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
